3588a9dbe4bf9f7f29de6ece85d9f52902a2c22d
Christian Wilhelm <c_wilhelm@mailbox.org>
2023-01-03 17:48:52 +0100

    Replace bookmark view with shared component Make BookmarkView public Removed TagElementComponent and LinkdingBookmarkView

--- BookmarkCompanion/integrations/linkding/ui/tags/LinkdingTagBookmarksView.swift
+++ BookmarkCompanion/integrations/linkding/ui/tags/LinkdingTagBookmarksView.swift
@@ -5,6 +5,7 @@
 
 import SwiftUI
 import Linkding
+import Shared
 
 struct LinkdingTagBookmarksView: View {
     @EnvironmentObject var bookmarkStore: LinkdingBookmarkStore
@@ -16,7 +17,7 @@ struct LinkdingTagBookmarksView: View {
             List {
                 if self.bookmarks().count > 0 {
                     ForEach(self.bookmarks()) { bookmark in
-                        LinkdingBookmarkView(bookmark: bookmark, showTags: false)
+                        BookmarkView(bookmark: bookmark, showTags: false)
                     }
                 } else {
                     Text("No bookmarks for this tag")
@@ -25,10 +26,10 @@ struct LinkdingTagBookmarksView: View {
             .navigationTitle("Bookmarks for \(self.title)")
     }
 
-    private func bookmarks() -> [LinkdingBookmarkEntity] {
-        guard let tagEntity = self.tag else {
-            return bookmarkStore.untagged
+    private func bookmarks() -> [Bookmark<Int>] {
+        let bookmarks = self.tag != nil ? self.tag!.bookmarks : self.bookmarkStore.untagged
+        return bookmarks.map {
+            Bookmark(id: $0.serverId, title: $0.displayTitle, url: $0.url, description: $0.websiteDescription, tags: $0.tags.map { Tag(id: $0.id, name: $0.name) })
         }
-        return tagEntity.bookmarks
     }
 }
--- Shared/components/bookmark/BookmarkView.swift
+++ Shared/components/bookmark/BookmarkView.swift
@@ -5,14 +5,28 @@
 
 import SwiftUI
 
-struct BookmarkView<ID: Hashable>: View {
+public struct BookmarkView<ID: Hashable>: View {
     var bookmark: Bookmark<ID>
-    var showLinkButton: Bool = true
-    var showTags: Bool = true
-    var showDescription: Bool = true
-    var tapHandler: (Bookmark<ID>) -> Void = { _ in }
+    var showLinkButton: Bool
+    var showTags: Bool
+    var showDescription: Bool
+    var tapHandler: (Bookmark<ID>) -> Void
+    
+    public init(
+        bookmark: Bookmark<ID>,
+        showLinkButton: Bool = true,
+        showTags: Bool = true,
+        showDescription: Bool = true,
+        tapHandler: @escaping (Bookmark<ID>) -> Void = { _ in }
+    ) {
+        self.bookmark = bookmark
+        self.showLinkButton = showLinkButton
+        self.showTags = showTags
+        self.showDescription = showDescription
+        self.tapHandler = tapHandler
+    }
 
-    var body: some View {
+    public var body: some View {
         HStack {
             VStack(alignment: .leading) {
                 Text(self.bookmark.title.trimmingCharacters(in: .whitespacesAndNewlines))
@@ -40,7 +54,7 @@ struct BookmarkView<ID: Hashable>: View {
 }
 
 struct BookmarkView_Previews: PreviewProvider {
-    static let bookmark = Bookmark(id: UUID(), title: "Dummy Bookmark Title", url: "https://www.github.com", description: "This is a dummy description", tags: [
+    static let bookmark = Bookmark<UUID>(id: UUID(), title: "Dummy Bookmark Title", url: "https://www.github.com", description: "This is a dummy description", tags: [
         Tag(id: UUID(), name: "tag-1"),
         Tag(id: UUID(), name: "tag-2")
     ])
