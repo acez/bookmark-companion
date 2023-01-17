485d2485d21d0042eb22f89d73cdfa63bf81a786
Christian Wilhelm <c_wilhelm@mailbox.org>
2023-01-17 19:40:06 +0100

    Add setting options to show description and tags in bookmark list of tag view

--- Linkding/settings/LinkdingSettingKeys.swift
+++ Linkding/settings/LinkdingSettingKeys.swift
@@ -23,6 +23,8 @@ public enum LinkdingSettingKeys: String {
     
     case tagFilterOnlyUsed = "linkding.tags.filter.onlyused"
     case tagSortOrder = "linkding.tags.sort.order"
+    case tagViewBookmarkDescription = "linkding.tags.view.bookmarkdescription"
+    case tagViewBookmarkTags = "linkding.tags.view.bookmarktags"
 
     case syncRunning = "linkding.sync.running"
     case syncHadError = "linkding.sync.error"
