656e639e59c9ba7d7f92a604dd1bbef7d41ca803
Christian Wilhelm <c_wilhelm@mailbox.org>
2023-05-06 00:19:32 +0200

    Simplify app structure
    Fix linkding model mapping

--- CompanionApplicationTests/ApplicationTests.swift
+++ CompanionApplicationTests/ApplicationTests.swift
@@ -6,7 +6,7 @@
 //
 
 import XCTest
-@testable import Application
+@testable import CompanionApplication
 
 final class ApplicationTests: XCTestCase {
 
--- CompanionApplicationTests/integrations/linkding/persistence/migrations/CoreDataMigration1To2Test.swift
+++ CompanionApplicationTests/integrations/linkding/persistence/migrations/CoreDataMigration1To2Test.swift
@@ -4,7 +4,7 @@
 //
 
 import XCTest
-@testable import Application
+@testable import CompanionApplication
 
 import CoreData
 
