05711871aa5b63902363ca9f1acbb4a54f5f4112
Christian Wilhelm <c_wilhelm@mailbox.org>
2023-07-03 23:40:34 +0200

    Make the tab view a more general view for bookmarks
    In order to only port less individual views to macOS and streamline the app, make the tag view more general.
    Make it able to view tags and also untagged bookmarks.
    Future additions might be to view all bookmarks regardless of tags.

--- CompanionApplication/integrations/linkding/ui/dashboard/tags/LinkdingTagsTabView.swift
+++ CompanionApplication/integrations/linkding/ui/dashboard/tags/LinkdingTagsTabView.swift
@@ -5,6 +5,21 @@
 
 import SwiftUI
 
+enum TagMenuItemType {
+    case untagged
+    case folder
+    case tag
+}
+
+struct TagViewMenuItem: Identifiable, Hashable {
+    var id: Self { self }
+    var name: String
+    var type: TagMenuItemType
+    var badge: Int?
+    var tag: LinkdingTagEntity?
+    var children: [TagViewMenuItem]?
+}
+
 struct LinkdingTagsTabView: View {
     @EnvironmentObject var tagStore: LinkdingTagStore
     @EnvironmentObject var bookmarkStore: LinkdingBookmarkStore
@@ -15,61 +30,78 @@ struct LinkdingTagsTabView: View {
     
     @AppStorage(LinkdingSettingKeys.tagSortOrder.rawValue, store: AppStorageSupport.shared.sharedStore) var sortOrder: SortOrder = .ascending
 
-    @State var selectedTag: LinkdingTagEntity? = nil
+    @State var selectedItem: TagViewMenuItem? = nil
     @State var tagSearchString: String = ""
     @State var createSheetOpen: Bool = false
     @State var filterSheetOpen: Bool = false
 
+    @State var tagGroupExpanded: Bool = true
+    
     @Binding var openConfig: Bool
     
     var body: some View {
-        NavigationView {
-            List {
-                if self.syncHadError {
-                    Section() {
-                        SyncErrorView(errorDetails: self.syncErrorMessage)
-                    }
+        NavigationSplitView(sidebar: {
+            if self.syncHadError {
+                Section() {
+                    SyncErrorView(errorDetails: self.syncErrorMessage)
                 }
-                if (self.tagSearchString == "" || "untagged".contains(self.tagSearchString.lowercased())) {
-                    NavigationLink(destination: LinkdingTagBookmarksView(tag: nil, title: "untagged")) {
-                        Text("untagged")
-                            .badge(self.bookmarkStore.untagged.count)
+            }
+            List(self.buildMenu(), children: \.children, selection: self.$selectedItem) { item in
+                if item.children != nil {
+                    HStack {
+                        Image(systemName: "folder")
+                        Text(item.name)
+                            .lineLimit(1)
                     }
+                } else {
+                    NavigationLink(value: item, label: {
+                        HStack {
+                            Image(systemName: "tag")
+                            Text(item.name)
+                                .badge(item.badge ?? 0)
+                                .lineLimit(1)
+                        }
+                    })
                 }
-                ForEach(self.filteredTags()) { tag in
-                    NavigationLink(destination: LinkdingTagBookmarksView(tag: tag, title: tag.name)) {
-                        Text(tag.name)
-                            .badge(tag.bookmarks.count)
+            }
+            .listStyle(.sidebar)
+            .navigationTitle("Tags")
+            .refreshable {
+                let syncClient = LinkdingSyncClient(tagStore: self.tagStore, bookmarkStore: self.bookmarkStore)
+                try? await syncClient.sync()
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading) {
+                    ConfigurationButton(actionHandler: {
+                        self.openConfig.toggle()
+                    })
+                }
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        self.filterSheetOpen = true
+                    }) {
+                        Image(systemName: "slider.horizontal.3")
+                    }
+                    Button(action: {
+                        self.createSheetOpen = true
+                    }) {
+                        Image(systemName: "plus")
                     }
                 }
             }
-                .listStyle(.insetGrouped)
-                .navigationTitle("Tags")
-                .refreshable {
-                    let syncClient = LinkdingSyncClient(tagStore: self.tagStore, bookmarkStore: self.bookmarkStore)
-                    try? await syncClient.sync()
+            .searchable(text: self.$tagSearchString)
+        }, detail: {
+            if selectedItem != nil {
+                if self.selectedItem!.type == .tag {
+                    LinkdingTagBookmarksView(tag: self.selectedItem!.tag, title: self.selectedItem!.name)
                 }
-                .toolbar {
-                    ToolbarItemGroup(placement: .navigationBarLeading) {
-                        ConfigurationButton(actionHandler: {
-                            self.openConfig.toggle()
-                        })
-                    }
-                    ToolbarItemGroup(placement: .navigationBarTrailing) {
-                        Button(action: {
-                            self.filterSheetOpen = true
-                        }) {
-                            Image(systemName: "slider.horizontal.3")
-                        }
-                        Button(action: {
-                            self.createSheetOpen = true
-                        }) {
-                            Image(systemName: "plus")
-                        }
-                    }
+                if self.selectedItem!.type == .untagged {
+                    LinkdingTagBookmarksView(tag: self.selectedItem!.tag, title: self.selectedItem!.name)
                 }
-                .searchable(text: self.$tagSearchString)
-        }
+            } else {
+                Text("Please select a tag")
+            }
+        })
             .sheet(isPresented: self.$createSheetOpen) {
                 CreateTagView()
             }
@@ -86,5 +118,18 @@ struct LinkdingTagsTabView: View {
 
                 return $0.name.compare($1.name, options: .caseInsensitive) == compareOrder
             }
+    }
+    
+    private func buildMenu() -> [TagViewMenuItem] {
+        var menu: [TagViewMenuItem] = []
+        
+        menu.append(TagViewMenuItem(name: "Untagged", type: .untagged, badge: self.bookmarkStore.untagged.count))
+        
+        let tags = self.filteredTags().map {
+            TagViewMenuItem(name: $0.name, type: .tag, badge: $0.bookmarks.count, tag: $0)
+        }
+        menu.append(TagViewMenuItem(name: "Tags", type: .folder, children: tags))
+        
+        return menu
     }
 }
